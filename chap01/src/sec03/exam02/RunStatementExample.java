9c54ce40d949d27d2e3da7c1a4c9b32eec3a29ff
An minsun <elleto123456@gmail.com>
2024-02-28 12:11:13 +0900

    chap01

---
+++ chap01/src/sec03/exam02/RunStatementExample.java
@@ -0,0 +1,13 @@
+package sec03.exam02;  //패키지
+
+public class RunStatementExample {  //클래스 선언부
+
+	public static void main(String[] args) {  //클래스 부록, main()메소드
+		int x = 1;
+		int y = 2;
+		int result = x + y;
+		System.out.println(result);
+
+	}
+
+}
